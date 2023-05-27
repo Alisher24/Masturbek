763b6a8a5dc38f4d28195ee1df712fe6cc592f7a
Alisher24 <101357390+Alisher24@users.noreply.github.com>
2023-05-27 22:38:24 +0700

    Delete StudentRecipe directory

--- StudentRecipe/Profile/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.shortcuts import render,redirect
-from django.views import View
-from globalUtils import HandleUploadedFile
-# Create your views here.
-def index(request):
-    return render(request, 'Profile/Profile.html')
