f6612d35b39b12ed650dbf58614222610387993c
Alisher <baiyskulov.alisher@gmail.com>
2023-05-30 23:41:10 +0700

    frst

--- Profile/views.py
+++ Profile/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from Auth.models import CustomerUser
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 def index(request):
     return render(request, 'Profile/Profile.html')
@@ -9,6 +11,7 @@ def edit_profile(request):
         edit_name = request.POST.get('edit_name')
         edit_phone = request.POST.get('edit_phone')
         edit_email = request.POST.get('edit_email')
+        edit_photo = request.FILES.get('edit_photo')
 
         # Обновление данных пользователя в базе данных
         user = request.user
@@ -20,6 +23,16 @@ def edit_profile(request):
         customer_user.first_name = edit_name
         customer_user.phone_number = edit_phone
         customer_user.email = edit_email
+
+        # Сохранение нового фото, если оно было выбрано
+        if edit_photo:
+            fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+            filename = fs.save(edit_photo.name, edit_photo)
+            customer_user.photo = fs.url(filename).lstrip('/').lstrip('media/')
+        else:
+            # Если новое фото не было выбрано, сохраняем прежнее фото
+            customer_user.photo = user.photo
+
         customer_user.save()
 
         return redirect('profile')  # Перенаправление на страницу профиля
